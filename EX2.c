a32f9782ded649b70252a63679b51a8ef8230e5a
JSkeerthana <keerthana.sjcse@ksrce.ac.in>
2017-06-14 15:50:14 +0530

    Add files via upload

---
+++ EX2.c
@@ -0,0 +1,13 @@
+    #include<stdio.h>
+    #include<conio.h>
+    void main(){
+      int i,fact=1,number;
+      printf("Enter a number: ");
+      scanf("%d",&number);
+
+      for(i=1;i<=number;i++){
+          fact=fact*i;
+      }
+      printf("Factorial of %d is: %d",number,fact);
+      getch();
+    }
