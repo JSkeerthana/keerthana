c03ad289c1f6ba132fa6fd15d2cb7cf27c4579f4
JSkeerthana <keerthana.sjcse@ksrce.ac.in>
2017-06-21 14:01:50 +0530

    Add files via upload

---
+++ eve1.java
@@ -0,0 +1,17 @@
+package emp;
+
+public class eve1 {
+	public static void main(String[] args)
+	{
+      int limit = 50;
+       System.out.println("Printing Even numbers between 1 and " + limit);
+       for(int i=1; i <= limit; i++){
+        if( i % 2 == 0){
+        System.out.print(i + " ");
+                }
+        }
+}
+}
+	
+
+
