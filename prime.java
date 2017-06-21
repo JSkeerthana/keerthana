17a81cf3a32a3edc38791525017c2bf6ea69dff6
JSkeerthana <keerthana.sjcse@ksrce.ac.in>
2017-06-21 14:07:33 +0530

    Add files via upload

---
+++ prime.java
@@ -0,0 +1,34 @@
+package emp;
+
+import java.util.Scanner;
+
+public class prime {
+	public static void main(String[] args)
+	{
+	 int num, i, count=0;
+     Scanner scan = new Scanner(System.in);
+		
+     System.out.print("Enter a Number : ");
+     num = scan.nextInt();
+		
+     for(i=2; i<num; i++)
+     {
+         if(num%i == 0)
+         {
+             count++;
+             break;
+         }
+     }
+     if(count == 0)
+     {
+         System.out.print("This is a Prime Number");
+     }
+     else
+     {
+         System.out.print("This is not a Prime Number");
+         scan.close();
+     }
+ }
+}
+
+
