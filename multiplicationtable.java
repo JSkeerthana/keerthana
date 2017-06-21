d3f73e091e6f800165ffeb0e6c30fe0fc0d3e53f
JSkeerthana <keerthana.sjcse@ksrce.ac.in>
2017-06-21 15:58:49 +0530

    Add files via upload

---
+++ multiplicationtable.java
@@ -0,0 +1,26 @@
+package guvitest;
+
+import java.util.Scanner;
+
+public class multiplicationtable {
+	public static void main(String[] args)
+	{
+        Scanner s = new Scanner(System.in);
+
+	System.out.print("Enter number:");        
+
+	int n=s.nextInt();
+
+        for(int i=1; i <= 10; i++)
+
+        {
+
+            System.out.println(n+" * "+i+" = "+n*i);
+            s.close();
+        }
+
+    }
+
+}
+	
+
